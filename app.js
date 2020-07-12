962867a7ae9bf7e714c04d6e7ce2cbe80d66d573
Noyan Alimov <noyan.alimov@gmail.com>
2020-07-12 23:43:14 +0100

    inital commit

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
+// Create Dino Constructor
 
-    // Create Dino Constructor
+function Dino(species, weight, height, diet, where, when, fact, image) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+  this.image = image;
+}
 
+// Create Dino Objects
 
-    // Create Dino Objects
+const triceratops = new Dino('Triceratops',
+                13000,
+                114,
+                'herbavor',
+                'North America',
+                'Late Cretaceous',
+                'First discovered in 1889 by Othniel Charles Marsh',
+                'triceratops.png'
+              );
 
+const trex = new Dino('Tyrannosaurus Rex',
+                11905,
+                144,
+                'carnivor',
+                'North America',
+                'Late Cretaceous',
+                'The largest known skull measures in at 5 feet long.',
+                'tyrannosaurus rex.png'
+              );
 
-    // Create Human Object
+// Create Human Object
 
-    // Use IIFE to get human data from form
+// Use IIFE to get human data from form
 
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Generate Tiles for each Dino in Array
 
+// Add tiles to DOM
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
